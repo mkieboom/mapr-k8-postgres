b45f7cf8a65710f6a1441892ac73330131dcda24
mkieboom <mkieboom@mapr.com>
2018-02-23 09:16:59 +0100

    Initial commit

--- launch.sh
+++ launch.sh
@@ -12,24 +12,24 @@ echo "Running Postgres launch script now."
 echo
 
 # Set access rights for postgres user on database location folder
-sudo chown -R postgres:postgres $PGDATA_LOCATION
+chown -R postgres:postgres $PGDATA_LOCATION
 
 # Set location of database directory
-sudo sed -ie "s|/var/lib/pgsql/data|$PGDATA_LOCATION|g" /usr/lib/systemd/system/postgresql.service
+sed -ie "s|/var/lib/pgsql/data|$PGDATA_LOCATION|g" /usr/lib/systemd/system/postgresql.service
 
 # Initdb
-sudo su postgres -c "/usr/bin/initdb --auth-host=md5 --auth-local=trust --pgdata=$PGDATA_LOCATION"
+su postgres -c "/usr/bin/initdb --auth-host=md5 --auth-local=trust --pgdata=$PGDATA_LOCATION"
 
 # Open Postgres from all client IP's
-sudo echo "host    all             all             0.0.0.0/0               md5" >> $PGDATA_LOCATION/pg_hba.conf
-sudo echo "listen_addresses = '*'" >> $PGDATA_LOCATION/postgresql.conf
+echo "host    all             all             0.0.0.0/0               md5" >> $PGDATA_LOCATION/pg_hba.conf
+echo "listen_addresses = '*'" >> $PGDATA_LOCATION/postgresql.conf
 
 # Launch Postgres shortly to create users and database provided
-sudo su postgres -c "/usr/bin/pg_ctl start --pgdata=$PGDATA_LOCATION -w"
-sudo su postgres -c "psql --command \"CREATE USER $PG_USER WITH SUPERUSER PASSWORD '$PG_DB';\""
-sudo su postgres -c "createdb -O $PG_USER $PG_DB"
-sudo su postgres -c "/usr/bin/pg_ctl stop --pgdata=$PGDATA_LOCATION -w"
+su postgres -c "/usr/bin/pg_ctl start --pgdata=$PGDATA_LOCATION -w"
+su postgres -c "psql --command \"CREATE USER $PG_USER WITH SUPERUSER PASSWORD '$PG_DB';\""
+su postgres -c "createdb -O $PG_USER $PG_DB"
+su postgres -c "/usr/bin/pg_ctl stop --pgdata=$PGDATA_LOCATION -w"
 
 # Launch Postgres to make it a long living container
 echo "Postgres server running."
-sudo su postgres -c "/usr/bin/postgres -D $PGDATA_LOCATION -h 0.0.0.0"
+su postgres -c "/usr/bin/postgres -D $PGDATA_LOCATION -h 0.0.0.0"
